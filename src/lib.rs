9d5c559e0bcb67c6b7b85c909e2c9c3f42086d00
daktic <gberg@dakticdigital.com>
2024-02-09 15:34:35 -0500

    Added a preliminary database structure

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, PartialEq, Hash)]
+struct Triple {
+    subject: String,
+    predicate: String,
+    object: String,
+}
+
+struct TripleStore {
+    triples: HashSet<Triple>,
+}
+
+impl TripleStore {
+    fn new() -> Self {
+        Self {
+            triples: HashSet::new(),
+        }
+    }
+
+    fn add(&mut self, triple: Triple) {
+        self.triples.insert(triple);
+    }
+
+    fn get(&self, subject: &str, predicate: &str, object: &str) -> Option<&Triple> {
+        self.triples.iter().find(|&triple| {
+            triple.subject == subject && triple.predicate == predicate && triple.object == object
+        })
+    }
+}
