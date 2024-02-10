f31b877485fc5da0a8294e779f1752c1aa0f467d
daktic <gberg@dakticdigital.com>
2024-02-10 12:54:23 -0500

    Adding some tests

--- src/lib.rs
+++ src/lib.rs
@@ -27,4 +27,75 @@ impl TripleStore {
             triple.subject == subject && triple.predicate == predicate && triple.object == object
         })
     }
+
+    pub fn query(&self, query: &str) -> Vec<&Triple> {
+        let mut result = Vec::new();
+        println!("Query: {}", query);
+        result
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut store = TripleStore::new();
+        store.add(Triple {
+            subject: "subject".to_string(),
+            predicate: "predicate".to_string(),
+            object: "object".to_string(),
+        });
+
+        assert_eq!(
+            store.triples,
+            vec![Triple {
+                subject: "subject".to_string(),
+                predicate: "predicate".to_string(),
+                object: "object".to_string(),
+            }]
+            .into_iter()
+            .collect()
+        );
+    }
+
+    #[test]
+    fn test_get() {
+        let mut store = TripleStore::new();
+        store.add(Triple {
+            subject: "subject".to_string(),
+            predicate: "predicate".to_string(),
+            object: "object".to_string(),
+        });
+
+        assert_eq!(
+            store.get("subject", "predicate", "object"),
+            Some(&Triple {
+                subject: "subject".to_string(),
+                predicate: "predicate".to_string(),
+                object: "object".to_string(),
+            })
+        );
+    }
+
+    #[test]
+    fn test_query() {
+        let mut store = TripleStore::new();
+        store.add(Triple {
+            subject: "subject".to_string(),
+            predicate: "predicate".to_string(),
+            object: "object".to_string(),
+        });
+
+        assert_eq!(
+            store.query("SELECT ?predicate ?object WHERE { subject}"),
+            vec![&Triple {
+                subject: "subject".to_string(),
+                predicate: "predicate".to_string(),
+                object: "object".to_string(),
+            }]
+        );
+    }
 }
